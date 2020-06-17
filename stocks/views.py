2ce5d6ff52813a5321b49bd410e2703009b89a19
siht <carlos.tz.00.00@gmail.com>
2020-06-17 00:03:09 -0500

    adapt view to bulk create

--- stocks/views.py
+++ stocks/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -7,3 +8,13 @@ from .serializers import ProductSerializer
 class BulkInsertProductView(CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(
+            data=request.data.pop('products'),
+            many=True
+        )
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({'status': 'OK'}, headers=headers)
