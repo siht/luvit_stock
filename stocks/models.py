345e50f69573dd25643d18ed1d93d1374152ce5d
siht <carlos.tz.00.00@gmail.com>
2020-06-17 20:31:41 -0500

    update model to avoid model validation

--- stocks/models.py
+++ stocks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     product_id = models.CharField(max_length=32)
-    name = models.CharField(max_length=55)
+    name = models.CharField(max_length=56)
     value = models.FloatField()
     discount = models.FloatField(null=True, blank=True)
     stock = models.IntegerField()
