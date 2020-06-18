1bf778de16597ac990b2c4064648e7fb10aa9d64
siht <carlos.tz.00.00@gmail.com>
2020-06-17 20:32:12 -0500

    write validation for data

--- stocks/serializers.py
+++ stocks/serializers.py
@@ -5,6 +5,7 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     id = serializers.CharField(source='product_id')
+
     class Meta:
         model = Product
         fields = (
@@ -14,3 +15,21 @@ class ProductSerializer(serializers.ModelSerializer):
             'discount',
             'stock',
         )
+    
+    def validate(self, data):
+        errors = []
+        if len(data['name']) < 3 or len(data['name']) > 55:
+            errors.append('Invalid product name')
+        if data['stock'] < 0:
+            errors.append('Invalid stock value')
+        if data['value'] <= 0 or data['value'] > 99999.9:
+            errors.append('Invalid value')
+        if data.get('discount', 0) > data['value']:
+            errors.append('Invalid discount value')
+        if errors:
+            error_message = {
+                'id': data['product_id'],
+                'errors': errors
+            }
+            raise serializers.ValidationError(error_message)
+        return data
