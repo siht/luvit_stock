35cfa1b8455af3ab9ca22d3f0e4b9f77a0a5054d
siht <carlos.tz.00.00@gmail.com>
2020-06-17 20:32:39 -0500

    write error response on fail

--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 
@@ -14,7 +15,25 @@ class BulkInsertProductView(CreateAPIView):
             data=request.data.pop('products'),
             many=True
         )
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response({'status': 'OK'}, headers=headers)
+        if(serializer.is_valid()):
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response({'status': 'OK'}, headers=headers)
+        list_errors = list(
+            map(lambda obj: {
+                'id': str(obj['id'][0]),
+                'errors': list(map(
+                    lambda e: str(e),
+                    obj['errors']
+                ))
+            }, serializer.errors)
+        )
+        error_response = {
+            'status': 'ERROR',
+            'products_report': list_errors,
+            'number_of_products_unable_to_parse': len(list_errors)
+        }
+        return Response(
+            error_response,
+            status=status.HTTP_422_UNPROCESSABLE_ENTITY
+        )
